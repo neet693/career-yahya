c59cbefba02773af90b4388132e3ec9c74eb5df2
unknown <neet693@gmail.com>
2025-11-05 11:21:34 +0700

    pake icon

--- app/components/navbarGlobal.js
+++ app/components/navbarGlobal.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function NavbarGlobal() {
   const pathname = usePathname();
@@ -16,7 +17,14 @@ export default function NavbarGlobal() {
         href="/"
         className="text-xl font-semibold text-blue-700 flex items-center gap-2"
       >
-        🎓 <span>Yahya Career</span>
+        <Image
+          src="/favicon.ico" // pakai favicon kamu
+          alt="Yahya Career Logo"
+          width={28}
+          height={28}
+          className="rounded-md"
+        />
+        <span>Yahya Career</span>
       </Link>
     </nav>
   );
